c9039f7ef8796c71894a60bdbfee18609558796a
Usman7472860 <muhammadusmanchachar54@gmail.com>
2026-06-09 11:18:15 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,15 @@
+const settings = {
+  botName: "US MOD MD",
+  botOwner: "USMAN KHAN CHACHAR",
+  ownerNumber: "923204822390",
+  owner: "923204822390",
+  packname: "US MOD MD",
+  author: "USMAN",
+  version: "1.0.0",
+  commandMode: "public",
+  prefix: ".",
+  storeWriteInterval: 10000,
+  maxStoreMessages: 20,
+};
+
+module.exports = settings;
